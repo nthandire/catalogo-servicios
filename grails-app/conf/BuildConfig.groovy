82e9577f4bb2cf1d61a0a83fa83663e4155f6dc4
Daniel Zamora <daniel.zamora.olvera@gmail.com>
2015-07-02 16:05:01 -0500

    Corriendo con BootStrap

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,6 +50,7 @@ grails.project.dependency.resolution = {
         runtime ":jquery:1.8.3"
         runtime ":resources:1.2"
         runtime ":twitter-bootstrap:2.3.2"
+        runtime ":fields:1.3"
 
         // Uncomment these (or add new ones) to enable additional resources capabilities
         //runtime ":zipped-resources:1.0"
