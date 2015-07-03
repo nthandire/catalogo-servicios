b632e13ff958a7c314abe70e987d53a5f8fc1442
Daniel Zamora <daniel.zamora.olvera@gmail.com>
2015-07-02 19:17:09 -0500

    Ya funciona con Spring Security - archivos nuevos

---
+++ grails-app/controllers/catalogo/servicios/SecureController.groovy
@@ -0,0 +1,11 @@
+package catalogo.servicios
+
+import grails.plugins.springsecurity.Secured
+
+@Secured(['ROLE_ADMIN'])
+class SecureController {
+
+    def index() {
+      render 'Secure access only'
+    }
+}
