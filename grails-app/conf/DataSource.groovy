90eb267daaba747a345b907bb67485fe8c2ee606
Daniel Zamora <daniel.zamora.olvera@inr.com>
2015-09-14 10:09:36 -0500

    Configuración de datos del INR.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,55 +1,55 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-    dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-    // url = "jdbc:h2:mem:informixMemDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-    url = "jdbc:h2:informixDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-}
 // dataSource {
-//     dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
-//     url = "jdbc:informix-sqli://192.168.10.1:1526/sast_dev:informixserver=ol_inrserver"
-//     driverClassName = "com.informix.jdbc.IfxDriver"
-//     dialect = "org.hibernate.dialect.InformixDialect"
-//     username = "informix"
-//     password = "informix"
 //     pooled = true
+//     driverClassName = "org.h2.Driver"
+//     username = "sa"
+//     password = ""
+//     dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//     // url = "jdbc:h2:mem:informixMemDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+//     url = "jdbc:h2:informixDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 // }
-dataSource_seguridad {
-    pooled = false
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-    dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-    url = "jdbc:h2:seguridadDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+dataSource {
+    dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+    url = "jdbc:informix-sqli://192.168.10.1:1526/sast_dev:informixserver=ol_inrserver"
+    driverClassName = "com.informix.jdbc.IfxDriver"
+    dialect = "org.hibernate.dialect.InformixDialect"
+    username = "informix"
+    password = "informix"
+    pooled = true
 }
 // dataSource_seguridad {
-//     dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
-//     url = "jdbc:informix-sqli://192.168.10.12:1527/saihweb:informixserver=ol_adminserver"
-//     driverClassName = "com.informix.jdbc.IfxDriver"
-//     dialect = "org.hibernate.dialect.InformixDialect"
-//     username = "informix"
-//     password = "informix"
-//     pooled = true
+//     pooled = false
+//     driverClassName = "org.h2.Driver"
+//     username = "sa"
+//     password = ""
+//     dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//     url = "jdbc:h2:seguridadDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 // }
-dataSource_almacen {
-    pooled = false
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-    dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
-    url = "jdbc:h2:almacenDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
+dataSource_seguridad {
+    dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+    url = "jdbc:informix-sqli://192.168.10.12:1527/saihweb:informixserver=ol_adminserver"
+    driverClassName = "com.informix.jdbc.IfxDriver"
+    dialect = "org.hibernate.dialect.InformixDialect"
+    username = "informix"
+    password = "informix"
+    pooled = true
 }
 // dataSource_almacen {
-//     dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
-//     url = "jdbc:informix-sqli://192.168.10.1:1526/almacenes:informixserver=ol_inrserver" // TODO: _dev es la BD
-//     driverClassName = "com.informix.jdbc.IfxDriver"
-//     dialect = "org.hibernate.dialect.InformixDialect"
-//     username = "informix"
-//     password = "informix"
-//     pooled = true
+//     pooled = false
+//     driverClassName = "org.h2.Driver"
+//     username = "sa"
+//     password = ""
+//     dbCreate = "update" // one of 'create', 'create-drop', 'update', 'validate', ''
+//     url = "jdbc:h2:almacenDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
 // }
+dataSource_almacen {
+    dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+    url = "jdbc:informix-sqli://192.168.10.1:1526/almacenes:informixserver=ol_inrserver" // TODO: _dev es la BD
+    driverClassName = "com.informix.jdbc.IfxDriver"
+    dialect = "org.hibernate.dialect.InformixDialect"
+    username = "informix"
+    password = "informix"
+    pooled = true
+}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
