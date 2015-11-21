bc4b5c331ba3604bf5e4f3ecb1bc6cd04b27031a
Daniel Zamora <daniel.zamora.olvera@inr.com>
2015-11-20 18:17:36 -0600

    Parcial.

--- grails-app/services/mx/gob/inr/catservicios/ServiciosService.groovy
+++ grails-app/services/mx/gob/inr/catservicios/ServiciosService.groovy
@@ -15,13 +15,25 @@ class ServiciosService {
 
     def codigos = ResguardoEntrega.findAllByCodigoLike("515%").collect {it.id}
     log.debug("número de codigos = ${codigos.size()}")
+    log.debug("codigos = ${codigos}")
+    log.debug("esta (12 as Long) = ${(12 as Long) in codigos}")
+    log.debug("esta (144 as Long) = ${(144 as Long) in codigos}")
+    log.debug("esta 144 = ${codigos[143] in codigos}")
+    log.debug("tipo de 144 = ${codigos[143].getClass()}")
+    log.debug("tercero = ${codigos[2]}")
+    log.debug("143 = ${codigos[143]}")
+    log.debug("200 = ${codigos[200]}")
+    log.debug("290 = ${codigos[290]}")
+    log.debug("295 = ${codigos[295]}")
 
+    def MAX_EQUIPOS = 2 // Es el consecutivo maximo a considerar,
       // TODO. aplicar un orden -  order("inventario","asc")
     def query =
-       "  from ResguardoEntregaDetalle as d   " +
-       " where d.id in (:codigos)             " +
-       "   and (serie like :serie             " +
-       "        or inventario = :inventario)  "
+       "  from ResguardoEntregaDetalle as d     " +
+       " where d.id in (:codigos)               " +
+       "   and d.consecutivo <= ${MAX_EQUIPOS}  " +
+       "   and (serie like :serie               " +
+       "        or inventario = :inventario)    "
     log.debug("query = $query")
 
     log.debug("inventario = ${((term.isNumber())?term.toLong():0)}")
